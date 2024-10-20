a2ebfc471b1f05c0fb6a6bf574c483b9ba36c32c
franvpjr <franvpjr@hotmail.com>
2024-10-20 11:20:16 -0300

    Add files via upload

---
+++ prova02.py
@@ -0,0 +1,8 @@
+numero = int(input('digite um numero:'))
+
+if numero > 0:
+    print(f'o numero {numero} é positivo')
+elif numero < 0:
+    print(f'o numero {numero} é negativo')
+else:
+    print(f'o numero {numero} é zero')
